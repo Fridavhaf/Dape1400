0a9ca7a1302179bfeae727adf8206fb321623f24
Frida von Hafenbrädl <fridavhaf@gmail.com>
2023-09-03 14:34:17 +0200

    navn og adresse printes

--- Strenger/src/strenger/Strenger.java
+++ Strenger/src/strenger/Strenger.java
@@ -1,4 +1,12 @@
 package strenger;
 
 public class Strenger {
+    public static void main(String[] args) {
+        String navn = "Frida von Hafenbrädl";
+        String adresse = "Seljeveien 37";
+        String ut = navn + adresse;
+        System.out.print(ut);
+    }
+
+
 }
