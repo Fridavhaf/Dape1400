30b3c9d3654c861bba4f9e484911ac58e7b553e8
Frida von Hafenbrädl <fridavhaf@gmail.com>
2023-09-04 08:53:36 +0200

    navn og adresse printes

--- Strenger/src/strenger/Strenger.java
+++ Strenger/src/strenger/Strenger.java
@@ -1,12 +1,22 @@
 package strenger;
 
+import javax.swing.*;
+
 public class Strenger {
-    public static void main(String[] args) {
-        String navn = "Frida von Hafenbrädl";
+    public static void main(String [] args){
+        String navn = JOptionPane.showInputDialog("Hva heter du?");
+        System.out.print(navn);
+        String etternavn = "Bamsemums";
         String adresse = "Seljeveien 37";
-        String ut = navn + " " + adresse;
-        System.out.print(ut);
+        String ut = "\n" + etternavn +"\n"+ adresse;
+        System.out.println(ut);
+
     }
-
-
 }
+//Her har jeg laget et program hvor man kan gi input til programmet gjennom en message box
+/*Her er spørsmålet "Hva heter du?" besvart med "Frida von Hafenbrädl
+Swing er en gammel funksjon, men det kan gjøre input og output litt mer synlig
+nå i opplæringsfasen.
+ */
+
+
