1f26342d77648a50dc2819f8bb5037386dcf62fc
Frida von Hafenbrädl <fridavhaf@gmail.com>
2023-09-04 12:13:30 +0200

    personinfo

---
+++ VariablerOppgave/src/variablerOppgave/VariablerOppgave.java
@@ -0,0 +1,21 @@
+package variablerOppgave;
+
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class VariablerOppgave {
+    public static void main(String[] args) {
+        String innFornavn = showInputDialog("Skriv inn fornavn");
+        String innEtternavn = showInputDialog("Skriv inn etternavn");
+        showMessageDialog(null, innFornavn+" er fornavnet"+"\n"+innEtternavn+" "+"er etternavnet");
+        String innAlder = showInputDialog("Skriv inn alder");
+        showMessageDialog(null, "Alderen til"+" "+innFornavn+" "+innEtternavn+" "+"er"+" "+innAlder+" "+"år");
+        String innAdresse = showInputDialog("Skriv inn adresse");
+        String innPostnr = showInputDialog("Skriv inn postnummer");
+        String innPostadresse = showInputDialog("Skriv inn postadresse");
+        showMessageDialog(null, "Navn : "+innFornavn+" "+innEtternavn+"\n"+"Adresse : "+innAdresse
+        +"\n"+"Postnummer : "+innPostnr+"\n"+"Poststed : "+innPostadresse+"\n"+"Alder : "+innAlder);
+
+
+    }
+}
