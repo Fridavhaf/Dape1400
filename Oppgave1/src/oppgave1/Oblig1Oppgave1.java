4379c94e702390baf6212563a6e49b00b81a5e5b
Frida von Hafenbrädl <143392748+Fridavhaf@users.noreply.github.com>
2023-10-04 14:34:19 +0200

    Merge remote-tracking branch 'origin/main'

--- Oppgave1/src/oppgave1/Oblig1Oppgave1.java
+++ Oppgave1/src/oppgave1/Oblig1Oppgave1.java
@@ -1,2 +1,17 @@
-package oppgave1;public class Oblig1Oppgave1 {
+package oppgave1;
+
+public class Oblig1Oppgave1 {
+    public static void main (String [] args){
+        double radius = 23.5;
+
+        double diameter = Sirkel.beregnDiameter(radius);
+        double omkrets = Sirkel.beregnOmkrets(radius);
+        double areal = Sirkel.beregnAreal(radius);
+
+        System.out.println("Diameteren til en sirkel med radius "+radius+" er "+String.format("%.2f", diameter));
+        System.out.println("Omkretsen av en sirkel med radius "+radius+" er "+String.format("%.2f", omkrets));
+        System.out.println("Arealet til en sirkel med radius "+radius+" er "+String.format("%.2f", areal));
+
+
+    }
 }
--- Oppgave1/src/oppgave1/Sirkel.java
+++ Oppgave1/src/oppgave1/Sirkel.java
@@ -1,2 +1,20 @@
-package oppgave1;public class Sirkel {
+package oppgave1;
+
+public class Sirkel { //Den er public og kan hentes fra en annen klasse
+
+
+    public static double beregnDiameter(double radius){
+        double diameter = radius*2;
+        return diameter;
+    }
+
+    public static double beregnOmkrets(double radius){
+        double omkrets = radius*Math.PI*2; //Math.PI er en konstant for pi
+        return omkrets;
+    }
+
+    public static double beregnAreal (double radius){
+        double areal = Math.PI*Math.pow(radius, 2); //Math.pow opphøyer radiusen i 2.
+        return areal;
+    }
 }
