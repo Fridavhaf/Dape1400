19b66074a23e84778a0383db5cdea97552e35275
Frida von Hafenbrädl <143392748+Fridavhaf@users.noreply.github.com>
2023-10-04 14:34:19 +0200

    Merge remote-tracking branch 'origin/main'

--- Oppgave1/src/oppgave1/Oblig1Oppgave1.java
+++ Oppgave1/src/oppgave1/Oblig1Oppgave1.java
@@ -1,2 +1,27 @@
-package oppgave1;public class Oblig1Oppgave1 {
+//STUDENTNUMMER OG FULLT NAVN PÅ DELTAKERE
+
+package oppgave1;
+
+public class Oblig1Oppgave1 { //Her har vi klassen hvor vi skal kjøre programmet
+    public static void main (String [] args){ //Denne main-metoden kjører programmet
+        double radius = 23.5; //Vi må definere radius, som er parameter for alle metodene i Sirkel-klassen
+
+        /*Vi har statiske metoder i Sirkel-klassen, og det betyr at vi ikke trenger å instansiere et objekt av type Sirkel
+        *Vi kan hente metodene kun ved Klassenavn.metodeNavn(parameter).
+        *Når vi videre skriver inn parameteren trenger vi kun navnet, ikke type (double), det er allerede definert*/
+
+        double diameter = Sirkel.beregnDiameter(radius);
+        double omkrets = Sirkel.beregnOmkrets(radius);
+        double areal = Sirkel.beregnAreal(radius);
+        /*Her har vi definert diameter, omkrets og areal av type double, og hver av de kaller på hver sine metoder fra
+        Sirkel-klassen. Det vil si at variablene nå er definert med formlene fra metoden og verdien for radius som vi
+        definerte ovenfor.*/
+
+
+        //Videre kjører vi koden fra metodene. Det vi si at formlene blir beregnet og svaret på disse blir retunert til oss:
+        System.out.println("Diameteren til en sirkel med radius "+radius+" er "+String.format("%.2f", diameter));
+        System.out.println("Omkretsen av en sirkel med radius "+radius+" er "+String.format("%.2f", omkrets));
+        System.out.println("Arealet til en sirkel med radius "+radius+" er "+String.format("%.2f", areal));
+        //String.format("%.2f", variabel) gjør at variabelen av type double blir oppgitt med to desimaler.
+    }
 }
--- Oppgave1/src/oppgave1/Sirkel.java
+++ Oppgave1/src/oppgave1/Sirkel.java
@@ -1,2 +1,30 @@
-package oppgave1;public class Sirkel {
+//STUDENTNUMMER OG FULLT NAVN PÅ DELTAKERE
+
+package oppgave1;
+
+public class Sirkel { //Jeg lager en public class for Sirkel, public betyr at den kan hentes fra en annen klasse
+
+
+/*Vi trenger ikke å definere variablene diameter, omkrets og areal her, det gjør vi inne i metodene */
+
+
+
+    //Metode som beregner diameteren til sirkelen. Innparameteren vi trenger for å kjøre koden er radius:
+    public static double beregnDiameter(double radius){ //Innparameter radius er av type double, fordi vi skal regne med den
+        double diameter = radius*2; //Vi definerer diameter av typen double med formelen for å beregne diameter
+        return diameter; /*Vi vil at metoden skal returnere svaret for hva som blir diameteren, når radius er definert i den andre klassen,
+        og vi kjører System.out.println*/
+    }
+
+    //Så oppretter vi en metode for å beregne omkrets, vi trenger innparameter radius av type double:
+    public static double beregnOmkrets(double radius){
+        double omkrets = radius*Math.PI*2; //Math.PI er en konstant for pi. omkrets defineres her av typen double med formelen for omkrets.
+        return omkrets; //Vi vil at metoden skal returnere svaret for hva som blir omkrets når radius er oppgitt
+    }
+
+    //Neste er metode for å beregne areal, med innparameter radius av type double:
+    public static double beregnAreal (double radius){
+        double areal = Math.PI*Math.pow(radius, 2); //Math.pow opphøyer radiusen i 2. Variabel areal defineres
+        return areal; //denne koden oppgir hva arealet av en sirkel er, ut i fra sirkelens radius
+    }
 }
