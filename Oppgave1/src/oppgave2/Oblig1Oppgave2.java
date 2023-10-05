ddb99e460095848edae984b9cef876be49f3d9ef
Frida von Hafenbrädl <143392748+Fridavhaf@users.noreply.github.com>
2023-10-05 17:03:22 +0200

    Merge remote-tracking branch 'origin/main'

--- Oppgave1/src/oppgave2/Oblig1Oppgave2.java
+++ Oppgave1/src/oppgave2/Oblig1Oppgave2.java
@@ -1,4 +1,87 @@
+//STUDENTNUMMER OG FULLT NAVN PÅ ALLE DELTAKERE
+
 package oppgave2;
 
+import javax.swing.*;
+
 public class Oblig1Oppgave2 {
-}
+    public static void main(String[] args) {
+        //Siden vi skal ha input via bokser så må vi sette opp dette først
+
+        //Først setter vi inn data via input-bokser for vare 1:
+        String navn1 = JOptionPane.showInputDialog("Skriv inn varens navn");
+        String innAntall1 = JOptionPane.showInputDialog("Skriv inn antall varer");
+
+        /*Siden antall må være på desimalform, skal vi prøve å konvertere strengen til en desimal via try.
+        Vi setter også inn en catch, som kjøres dersom input ikke er mulig å konvertere.
+        Dette skjer for eksempel hvis det settes inn bokstaver eller tegn i input-boksen*/
+
+        int antall1;
+        try {
+            antall1 = Integer.parseInt(innAntall1);
+        } catch (Exception e) {
+            antall1 = 0;
+            JOptionPane.showMessageDialog(null,"Dette er ikke et heltall," +
+                    "\nAntall er satt til 0");
+        }
+        //Dersom catch kjøres settes antall til 0 og det vil komme en meldingsboks om at dette skjer.
+
+
+        //Videre oppretter vi en input-boks for pris
+
+        String innPris1 = JOptionPane.showInputDialog("Skriv inn prisen på varen");
+        //Vi prøver å konvertere innPris1 til en double pris1, hvis ikke det går setter catch prisen til null
+        double pris1;
+        try {
+                pris1 = Double.parseDouble(innPris1);
+            } catch (Exception e) {
+                pris1 = 0;
+                JOptionPane.showMessageDialog(null, "Dette er ikke et tall"+
+                        "\nPrisen er satt til null");
+               }
+
+
+        //Så gjør vi det samme for vare 2:
+        String navn2 = JOptionPane.showInputDialog("Skriv inn navnet på en annen vare");
+        String innAntall2 = JOptionPane.showInputDialog("Skriv inn antall av denne varen");
+        int antall2;
+        try{
+            antall2 = Integer.parseInt(innAntall2);
+        } catch (Exception e){
+            antall2 = 0;
+            JOptionPane.showMessageDialog(null, "Dette er ikke et heltall \nAntall er satt til null");
+        }
+        String innPris2 = JOptionPane.showInputDialog("Skriv inn prisen på varen");
+        double pris2;
+        try{
+            pris2 = Double.parseDouble(innPris2);
+        } catch (Exception e){
+            pris2 = 0;
+            JOptionPane.showMessageDialog(null, "Dette er ikke et tall\nPris er satt til null");
+        }
+
+        /*Vi oppretter to objekter, vare1 og vare2. Nå kalles det på konstruktøren, og vi må sette inn attributter
+        Disse attributtene er allerede definert via input-boksene*/
+
+
+        Vare vare1 = new Vare(navn1, antall1, pris1);
+        Vare vare2 = new Vare(navn2, antall2, pris2);
+
+
+
+        /*Videre oppretter vi variabel totalPris av typen double.
+        Så henter vi metoden beregnTotalpris fra "public class Vare".
+        Denne metoden bruker vi for å beregne vare1 sin totalpris og vare2 sin totalpris*/
+
+        double totalPris = (vare1.beregnTotalpris()) + (vare2.beregnTotalpris());
+        //Her blir totalprisen for vare1 og vare2 addert sammen, siden vi vil finne totalprisen av begge varene
+
+        String totalPrisMedToDesimaler = String.format("%.2f", totalPris);
+        //Det kan være greit å sette en restriksjon på to desimaler når det er snakk om pris
+
+        System.out.println("Totalprisen ble : " + totalPrisMedToDesimaler + " kr.");
+        //Den siste koden kjører ut totalprisen for begge varene basert på pris og antall for hver av de.
+
+        }
+    }
+
--- Oppgave1/src/oppgave2/Vare.java
+++ Oppgave1/src/oppgave2/Vare.java
@@ -1,7 +1,98 @@
+//STUDENTNUMMER OG FULLT NAVN PÅ ALLE DELTAKERE
+
 package oppgave2;
 
-public class Vare {
+public class Vare { //Vi oppretter en public class med navn Vare. Den heter altså det samme som Java-filen
+
+
+    //Videre har vi attributtene i klassen:
+
+    //Vi definerer variabel navn av type String
     private String navn;
-    private int tall;
+
+    //Vi definerer variabelen tall av type int
+    private int antall;
+
+    //Vi definerer variabelen pris av type double
     private double pris;
+
+    /*Disse variablene er private. Det vil si at de bare kan hentes fra/brukes i denne klassen.
+    Men ved get/set kan vi få brukt de fra en annen klasse allikevel.*/
+
+
+    //Etter attributtene setter vi en konstruktør. Dette er en metode med samme navn som klassen, må dermed ha stor forbokstav
+    public Vare (String navn, int antall, double pris) {
+        //Konstruktøren heter Vare og inneholder alle attributtene
+
+        this.navn = navn;
+        this.antall = antall;
+        this.pris = pris;
+
+        /*med this-kommandoen etterfulgt av .variabelnavn velger vi variabelen som er definert i denne klassen
+        Vi setter variablene navn, tall og pris i klassen til henholdsvis å være like attributtene i konstruktøren*/
+    }
+
+    /*Vi kaller på denne konstrukøtrn når vi senere oppretter et objekt av typen Vare.
+    Konstruktøren gjør at vi må sette verdier til attributtene med en gang.*/
+
+    /*Siden variablene i klassen er private, så oppretter vi nå "get"/"set"-metoder.
+    Disse kan vi hente ut data og lagre data i.
+    Da blir det mulig å bruke variablene fra en annen klasse, selv om variablene er private.
+    "get"/"set"-metoder kan vi også bruke til å sjekke dataen som legges inn i parameterene.
+    I denne oppgaven skal vi sørge for at attributtene antall og pris blir satt til større enn null*/
+
+
+
+    /*Vi oppretter en "set"-metode for navn. Metoden er public fordi den skal hentes fra en annen klasse.
+    Metoden er void, fordi den ikke skal returnere noe.
+    Metoden setNavn inneholder en innparameter; strengen navn. Metoden skal ta inn parameter for attributten navn av type String.
+    Vi setter this.navn = navn; for å sette metodens attributt navn lik klassens attributt navn.*/
+    public void setNavn(String navn){
+        this.navn = navn;
+    }
+
+    /*Videre oppretter vi en "get"-metode som skal returnere dataen som ble lagt inn i navn via "set"-metoden.*/
+    public String getNavn(){
+        return navn;
+    }
+
+    /*Videre oppretter vi "set"/"get"-metoder for antall. Forskjellen her er at vi at variabelen er av typen int.
+    Det er derfor viktig å definere det i innparameter i "set"-metoden og før metode-navnet i "get"-metoden slik at
+    det blir satt inn data av type int og retunert data av type int.*/
+    public void setAntall(int antall){
+        if (antall>0) {
+            this.antall = antall;
+            /*Hvis antall blir satt til større en null blir kommandoen kjørt.
+            Variabelen i klassen blir ikke tilegnet verdi dersom inndataen ikke er en int>0
+             */
+        }
+    }
+    public int getAntall(){
+            return antall;
+    }
+    public void setPris(double pris){
+        if (pris>0) {
+            this.pris = pris;
+        }
+    }
+    public double getPris(){
+            return pris;
+        }
+
+
+        //Videre trenger vi en metode for å regne beregne varens totalpris
+
+
+    public double beregnTotalpris(){
+        //Vi trenger ikke noen innparameter, vi har dataen vi trenger
+
+        double totalpris;
+        //Vi definerer variabelen pris.
+
+        totalpris = getAntall()*getPris();
+        //Vi setter pris lik formelen for å beregne pris
+
+        return totalpris;
+        //Vi vil ha svaret på formelen/prisens verdi oppgitt når vi kaller på metoden
+    }
 }
